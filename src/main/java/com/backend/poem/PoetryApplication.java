4711c7eb4a366ea3b772fcc20f403795ac8073a1
DESKTOP-TV0KOIN\Gamze GÜN <metin.gun@defne.com.tr>
2021-07-19 15:01:10 +0300

    no message

--- src/main/java/com/backend/poem/PoetryApplication.java
+++ src/main/java/com/backend/poem/PoetryApplication.java
@@ -2,12 +2,17 @@ package com.backend.poem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PoetryApplication {
+public class PoetryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoetryApplication.class, args);
 	}
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PoetryApplication.class);
+	}
 }
